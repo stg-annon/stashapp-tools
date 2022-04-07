7d2c0c4677edeb00f2802bcb0767ca76d2051ffb
stg-annon <14135675+stg-annon@users.noreply.github.com>
2022-04-07 12:29:22 -0400

    add version placeholder

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="stashapp-tools",
-    version="0.1.1",
+    version="main",
     description="A python library for interfacing with a stashapp's API",
     long_description=README,
     long_description_content_type="text/markdown",
